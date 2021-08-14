baefcaf96e19f80592b3ddb21fabf0ab7d7eb073
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-08-15 00:03:38 +0200

    feat: add render to Error404Page component with style

--- src/pages/Error404Page/Error404Page.jsx
+++ src/pages/Error404Page/Error404Page.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import "./Error404Page.scss";
 
 class Error404Page extends Component {
   render() {
-    return <div>Error 404 page</div>;
+    return (
+      <main className="error404-page">
+        <span className="error404-page__title">404</span>
+        <p className="error404-page__info-text">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+        <NavLink activeClassName="active-link" to="/">
+          Retourner sur la page d'accueil
+        </NavLink>
+      </main>
+    );
   }
 }
 
