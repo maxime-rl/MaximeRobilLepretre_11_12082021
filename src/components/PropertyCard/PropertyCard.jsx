f231371cbb22271f534cca470936bd4eca48469a
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-08-15 00:58:36 +0200

    feat: add PropertyCard without style

---
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+class PropertyCard extends Component {
+  render() {
+    const { id, title, src } = this.props;
+
+    return (
+      <Link to={"/property-location/" + id}>
+        <article>
+          <h2>{title}</h2>
+          <img src={src} alt={title} />
+        </article>
+      </Link>
+    );
+  }
+}
+
+PropertyCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+};
+
+/* defaultProps on image source, test the rendering when the component will be
+hydrated from the data with PropertyList and built on the HomePage */
+PropertyCard.defaultProps = {
+  src: "https://via.placeholder.com/340/000000/FF6060/?text=Kasa",
+};
+
+export default PropertyCard;
