e9b39d0b172f3cbc36072dcbc753386335e72f04
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-08-18 23:09:38 +0200

    feat: add TagList component without style

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as NavBar } from "./NavBar/NavBar";
 export { default as PropertyCard } from "./PropertyCard/PropertyCard";
 export { default as PropertyList } from "./PropertyList/PropertyList";
 export { default as Banner } from "./Banner/Banner";
+export { default as TagList } from "./TagList/TagList";
