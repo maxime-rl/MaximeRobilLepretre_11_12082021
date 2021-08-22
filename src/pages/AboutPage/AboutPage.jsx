2c935c7af987f4b912a1ee3fd3c9c031f34fc375
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-08-22 18:47:14 +0200

    feat: add data for dropdown content AboutPage and build page with style

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import { Banner } from "../../components";
+import { Banner, Dropdown } from "../../components";
 import aboutBanner from "../../assets/about_banner.jpg";
+import { dropdownContentAboutPage } from "../../data/data";
+import "./AboutPage.scss";
 
 class AboutPage extends Component {
   render() {
@@ -9,6 +11,15 @@ class AboutPage extends Component {
         <header>
           <Banner src={aboutBanner} alt="Paysage nature" />
         </header>
+        <main className="about-main">
+          {dropdownContentAboutPage.map(({ title, content, index }) => (
+            <Dropdown
+              key={`${title}-${index}`}
+              header={title}
+              content={content}
+            />
+          ))}
+        </main>
       </>
     );
   }
